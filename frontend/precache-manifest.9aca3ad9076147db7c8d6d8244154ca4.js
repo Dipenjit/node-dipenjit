cdc6c4c49b478653b7b99843ea15a1900860fea6
Dipenjit <dipenjitds@gmail.com>
2020-08-17 18:43:25 +0530

    File commited
    File commited

---
+++ frontend/precache-manifest.9aca3ad9076147db7c8d6d8244154ca4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c702d3eec9f0dc9087b2b878baf6a4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dacf1f42f1216ed3acdd",
+    "url": "/static/css/main.d9529271.chunk.css"
+  },
+  {
+    "revision": "459ecf760bc8614b0bd4",
+    "url": "/static/js/0.00d51dd8.chunk.js"
+  },
+  {
+    "revision": "6a0ceec9e7f78aa694f4",
+    "url": "/static/js/10.d84a1989.chunk.js"
+  },
+  {
+    "revision": "84c92384e18ffe5ef4a6",
+    "url": "/static/js/11.ea7ae236.chunk.js"
+  },
+  {
+    "revision": "95e6544b6890d6a49f82",
+    "url": "/static/js/3.f625b573.chunk.js"
+  },
+  {
+    "revision": "fa1c0690e0123730a556",
+    "url": "/static/js/4.22144e8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/4.22144e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785e00d69507047409bb",
+    "url": "/static/js/5.1fd10cc1.chunk.js"
+  },
+  {
+    "revision": "4f727858cf146e7dcd71",
+    "url": "/static/js/6.f8e00e4f.chunk.js"
+  },
+  {
+    "revision": "b2fde05f2230ae070a4d",
+    "url": "/static/js/7.d750b8b5.chunk.js"
+  },
+  {
+    "revision": "33a1051823007d6680b6",
+    "url": "/static/js/8.eca5e07d.chunk.js"
+  },
+  {
+    "revision": "9f583dabe5d5f088ed4e",
+    "url": "/static/js/9.9e504624.chunk.js"
+  },
+  {
+    "revision": "dacf1f42f1216ed3acdd",
+    "url": "/static/js/main.faf23fcc.chunk.js"
+  },
+  {
+    "revision": "bbeec0597aa95bc7b6e3",
+    "url": "/static/js/runtime-main.3ea825c8.js"
+  },
+  {
+    "revision": "b3de6fee10ded34c09b528a8f1425186",
+    "url": "/static/media/backgroung.b3de6fee.jpg"
+  },
+  {
+    "revision": "51bc8de0f22b962b4881e4253644a990",
+    "url": "/static/media/dashboard_image.51bc8de0.jpg"
+  }
+]);
